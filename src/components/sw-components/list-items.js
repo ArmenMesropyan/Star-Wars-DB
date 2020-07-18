565161550c5a0dc94c184fb60630e04821e841d9
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-18 19:31:46 +0400

    Functions compozition created for incapsulate children prop

--- src/components/sw-components/list-items.js
+++ src/components/sw-components/list-items.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import { withData, ListItems } from '..';
 import { SWApi } from '../../services';
+import { ListItem } from '../list-item';
 
 const { getAllPlanets, getAllPeople, getAllStarships } = new SWApi();
 
-const PlanetList = withData(ListItems, getAllPlanets);
-const PeopleList = withData(ListItems, getAllPeople);
-const StarshipList = withData(ListItems, getAllStarships);
+const withChildren = (View, render) => {
+    return (props) => (
+        <View {...props}>
+            { render }
+        </View>
+    )
+}
+
+const renderName = ({name}) => <button>{name}</button>
+
+const PlanetList = withData(
+    withChildren(ListItems, renderName),
+    getAllPlanets
+)
+const PeopleList = withData(
+    withChildren(ListItems, renderName),
+    getAllPeople
+)
+const StarshipList = withData(
+    withChildren(ListItems, renderName),
+    getAllStarships
+)
 
 export { PlanetList, PeopleList, StarshipList };
