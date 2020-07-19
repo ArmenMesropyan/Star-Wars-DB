20d4c21d28656025120b667ad5e6edf1154eabf0
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-19 15:07:09 +0400

    People detail component data fixed

--- src/components/sw-components/people-detail.js
+++ src/components/sw-components/people-detail.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { ListItem, Record, ErrorBoundry, withSwapiAPI } from '..';
 
-const PlanetDetail = ({ itemID, swapiService: { getPlanetById, getImage } }) => (
+const PeopleDetail = ({ itemID, swapiService: { getPersonById, getImage } }) => (
     <ErrorBoundry>
         <ListItem itemID={itemID}
-            getData={getPlanetById}
-            getImage={(id) => getImage('planets', id)}>
-                <Record label="Climate" field="climate"/>
-                <Record label="Diameter" field="diameter"/>
-                <Record label="Gravity" field="gravity"/>
+            getData={getPersonById}
+            getImage={(id) => getImage('characters', id)}>
+                <Record label="Birth Year" field="birth_year"/>
+                <Record label="Gender" field="gender"/>
+                <Record label="Eye Color" field="eye_color"/>
         </ListItem>
     </ErrorBoundry>
 );
 
-export default withSwapiAPI(PlanetDetail);
+export default withSwapiAPI(PeopleDetail);
